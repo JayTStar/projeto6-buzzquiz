308d8d9973c4ee46c5ccef2e567746c7d3f1601d
Jorge <jsbatalha@gmail.com>
2022-02-16 18:19:11 -0300

    Ceating "changeScreen" function

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,29 @@
+
+function changeScreen(screen){
+    console.log(screen);
+    const home = document.querySelector("#screen-home");
+    const quizz = document.querySelector("#screen-quizz");
+    const create = document.querySelector("#screen-create");
+
+    home.classList.add("hidden");
+    quizz.classList.add("hidden");
+    create.classList.add("hidden");
+
+    switch(screen){
+        case "home":
+            home.classList.remove("hidden");
+        
+        case "quizz":
+            quizz.classList.remove("hidden");
+
+        case "create":
+            create.classList.remove("hidden");
+    }
+}
+
 // -------- MOSTRAR QUIZZES --------
 
 //-------- LÓGICA DO QUIZZ --------
 
 //-------- CRIAÇÃO DO QUIZZ --------
 
-let tituloQuizz, URLImagem, qtdPerguntas, qtdRespostas;
-
-const quizz = { title: tituloQuizz, image:  URLImagem}
